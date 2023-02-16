d40f5f0816b8615638424b49e12194c3d579d99e
larycoder <lenhuchuhiep99@gmail.com>
2023-02-16 10:58:47 +0700

    pp2023: this is template for practical 1

---
+++ 1.student.mark.py
@@ -0,0 +1,18 @@
+# DATA section
+
+# FUNCTION section
+def input_func():
+    pass
+
+def list_func():
+    pass
+
+# Test section
+if __name__ == "__main__":
+    print("Hello world")
+    print("Testing program:\n")
+    print("###### input_func ######")
+    input_func()
+
+    print("###### list_func ######")
+    list_func()
